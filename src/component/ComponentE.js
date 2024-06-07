06537c6c9f858234d5ceeb037388a2473888ca87
rishabhjaiswal155 <rishabhjaiswal155@gmail.com>
2024-06-07 09:58:43 +0530

    Simplifed using useContext() hook

--- src/component/ComponentE.js
+++ src/component/ComponentE.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React,{useContext} from 'react'
 import ComponentF from './ComponentF'
+import { ChannelContext, UserContext } from '../App'
 
 function ComponentE() {
+    const user=useContext(UserContext)
+    const channel=useContext(ChannelContext)
   return (
     <div>
-        <ComponentF/>
+        {/* <ComponentF/> */}
+        User Context value-{user}, Channel Context Value-{channel}
     </div>
   )
 }
